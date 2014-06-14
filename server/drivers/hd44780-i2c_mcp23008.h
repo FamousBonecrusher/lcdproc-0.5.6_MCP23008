40642e3b37453eeea436dddb38138d5ba0810032
FamousBonecrusher <FamousBonecrusher@gmail.com>
2014-06-14 20:31:17 +0200

    lcdproc-0.5.6, added support for MCP23008 via I2C
    initial revision

---
+++ server/drivers/hd44780-i2c_mcp23008.h
@@ -0,0 +1,9 @@
+#ifndef HD_I2C_H_MCP23008
+#define HD_I2C_H_MCP23008
+
+#include "lcd.h"					  /* for Driver */
+
+// initialise this particular driver
+int hd_init_i2c_mcp23008(Driver *drvthis);
+
+#endif
